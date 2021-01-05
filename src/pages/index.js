6fc353ca439f03c78e4f2c1f1f782501e222c4c0
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-01-05 09:47:32 -0300

    Deleting unnecessary files

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next.js Boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Next.js Boilerplate</h1>
+    </>
+  )
+}
