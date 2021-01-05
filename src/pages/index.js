47d86f18dee55f974bbb939a09662accb8d540cc
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-01-05 09:55:44 -0300

    Adding EditorConfig to the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ export default function Home() {
         <title>Next.js Boilerplate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Next.js Boilerplate</h1>
+      <main>
+        <h1>Next.js Boilerplate</h1>
+      </main>
     </>
   )
 }
